a12a3f5beb20246566b705bf09ef1c8438c6ab9e
Fidel Castro <fc.fidel1995@gmail.com>
2021-06-04 23:05:10 -0400

    added a few components to test react router

--- src/App.js
+++ src/App.js
@@ -1,9 +1,23 @@
 import React from 'react';
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import About from './components/About';
+import Home from './components/Home';
+import NotFound from './components/NotFound';
 
 export function App() {
     return (
-        <div>
-            <h1>Hi {new Date().toDateString()}</h1>
-        </div>
+        <Router>
+            <Switch>
+                <Route exact path="/">
+                    <Home />
+                </Route>
+                <Route path="/about">
+                    <About />
+                </Route>
+                <Route path="*">
+                    <NotFound />
+                </Route>
+            </Switch>
+      </Router>
     );
 }
--- webpack.config.js
+++ webpack.config.js
@@ -2,35 +2,30 @@ const path = require('path');
 const miniCssExtractPlugin = require('mini-css-extract-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 module.exports = {
-    output: {
-        path: path.join(__dirname, '/dist'),
-        filename: 'index.bundle.js',
-    },
-    devServer: {
-        port: 3010,
-        watchContentBase: true,
-    },
-    module: {
-        rules: [
-            {
-                test: /\.(js|jsx)$/,
-                exclude: /node_modules/,
-                use: {
-                    loader: 'babel-loader'
-                }
-            },
-            {
-                test: /\.scss$/,
-                use: [
-                    MiniCssExtractPlugin.loader,
-                    'css-loader',
-                    'sass-loader'
-                ]
-            }
-        ]
-    },
-    plugins: [
-        new MiniCssExtractPlugin()
+  output: {
+    path: path.join(__dirname, "/dist"),
+    filename: "index.bundle.js",
+  },
+  devtool: "source-map",
+  devServer: {
+    port: 3010,
+    watchContentBase: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+        },
+      },
+      {
+        test: /\.scss$/,
+        use: [MiniCssExtractPlugin.loader, "css-loader", "sass-loader"],
+      },
+      { enforce: "pre", test: /\.js$/, loader: "source-map-loader" },
     ],
-
+  },
+  plugins: [new MiniCssExtractPlugin()],
 };
